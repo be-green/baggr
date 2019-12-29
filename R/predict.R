c945f76bc4175e3426d966770704e8a01095c95f
be-green <brice.c.green@gmail.com>
2019-12-28 16:51:31 -0800

    include hierarchical variance in posterior predictive distribution of rubin model

--- R/predict.R
+++ R/predict.R
@@ -1,26 +1,36 @@
+#' Models currently supported by baggr
+#' see \code{baggr()}
+baggr_models <- c("rubin", "quantiles", "mutau", "individual")
+
 #' Predict method for baggr objects
-#' @param x model to predict from
+#' @param x model fitted via baggr()
+#' @param newdata new data to use with the fitted model
+#' @param allow_new_levels whether to allow new groups or levels to be passed to the model.
 #' @param ... other arguments to pass to predict
 #' @export
-#' @details This is currently only implemented for the baggr model
-#' but could also be extended to the others. Currently a WIP, please
-#' report bugs.
+#' @details Predict responses based on the fitted model. Can be used
+#' with the data used to fit the model to perform posterior
+#' predictive checks, or with new data to generate new predictions.
+#' These will include not only the parameter uncertainty but also
+#' the variance that comes as a function of the likelihood. Because
+#' residual error is incorporated, it can be used for visual model
+#' criticism. For linear predictions (e.g. that only incorporate
+#' )
 predict.baggr <- function(x, newdata = NULL,
                           allow_new_levels = T, nsamples, ...) {
-  switch(x$model,
-         rubin = predict_rubin(x, newdata = newdata,
-                               allow_new_levels = allow_new_levels,
-                               nsamples = nsamples, ...),
-         quantiles = predict_quantiles(x, newdata = newdata,
-                                       allow_new_levels = allow_new_levels,
-                                       nsamples = nsamples,  ...))
+  do.call(paste0("predict_", x$model),
+          args = list(
+            x = x,
+            newdata = newdata,
+            allow_new_levels = allow_new_levels,
+            nsamples = nsamples,
+            ...)
+          )
 }
 
 #' Make model matrix for the rubin data
 #' @importFrom reshape2 dcast
-#' @param x model to get data from
-#' @param newdata new data to use with model
-#' @param allow_new_levels whether to allow for unobserved groups
+#' @inheritParams predict.baggr
 rubin_data <- function(x, newdata = NULL, allow_new_levels = T) {
   if(x$model != "rubin") {
     stop("Model must be type Rubin.")
@@ -67,7 +77,7 @@ rubin_data <- function(x, newdata = NULL, allow_new_levels = T) {
 #' Predict function for the rubin model
 #' @importFrom rstan extract
 #' @param x model to predict from
-#' @param newdata new data to predict, defaults to NULL
+#' @param newdata new data to predict not allowed for rubin model (yet)
 #' @param allow_new_levels allow the predictive of new, unobserved groups
 #' @param nsamples number of samples to predict
 predict_rubin <- function(x,
@@ -75,24 +85,34 @@ predict_rubin <- function(x,
                           allow_new_levels = T,
                           nsamples,
                           ...) {
+
+  if(!is.null(newdata)){
+    stop("New data not currently allowed for the rubin model. ")
+  }
+
   pred_data <- rubin_data(x, newdata)
 
   sigmas <- sapply(x$data$se, rep, times = nsamples)
 
   params <- rstan::extract(x$fit, c("tau","sigma_tau","tau_k","eta"))
 
-  tau_k <- params$tau_k[1:nsamples,]
-  eta <- params$eta[1:nsamples,]
+  tau_k <- params$tau_k[1:nsamples, ]
+  eta <- params$eta[1:nsamples, ]
 
-  tau <- params$tau[1:nsamples]
-  sigma_tau <- params$sigma_tau[1:nsamples]
+  tau <- params$tau[1:nsamples, ]
+  sigma_tau <- params$sigma_tau[1:nsamples, ]
 
   means <- cbind(tau, eta)
+
+  # measurement effect variance + treatment effect variance
+  # I think this was wrong before
+  full_sigmas <- sqrt(sigmas^2 + sigma_tau^2)
 
   pred_means <- pred_data %*% t(means)
 
   epsilon <- rnorm(length(sigmas),
-                   mean = 0, sigmas)
+                   mean = 0,
+                   full_sigmas)
 
   pp_dist <- pred_means + epsilon
 
@@ -114,15 +134,20 @@ get_y <- function(x) {
          rubin = "tau",
          mutau = "tau",
          quantiles = stop_not_implemented(),
-         full = stop_not_implemented()
+         individual = stop_not_implemented()
          )
 }
 
 #' Posterior predictive checks for baggr model
 #' @import bayesplot
-#' @param x Model to check
+#' @param x model fit by baggr()
 #' @param type type of pp_check. For a list see \link{bayesplot::available_ppc}
-#' @param nsamples number of samples to compare
+#' @param nsamples number of samples to draw for the comparison
+#' @details Posterior predictive checks are a form of model criticism
+#' that compares the likelihood based simulations from the posterior
+#' predictive distribution to observed data. Given a model fit by
+#' baggr, you can compare kernel density estimates, histograms, and much
+#' more. For full details, please refer to the bayesplot package.
 #' @export
 pp_check.baggr <- function(x, type = "dens_overlay", nsamples = 40) {
   pp_fun <- getFromNamespace(paste0("ppc_",type),ns = "bayesplot")
@@ -131,3 +156,4 @@ pp_check.baggr <- function(x, type = "dens_overlay", nsamples = 40) {
   yrep <- predict(x, nsamples = nsamples)
   pp_fun(y, yrep)
 }
+
